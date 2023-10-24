201b5854101143c8ce154aa97f3896786e452b7e
vididvidid <vididvidid@gmail.com>
2023-10-25 01:14:16 +0530

    full create user authentication setup is complete now we move forward to JWT

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
-const sequelize = require('./dbWithSequelize');
+const sequelize = require("./dbWithSequelize");
 //const {createSqlConnection} = require('./db');
-const express = require('express')
+const express = require("express");
 
 //defining port for express
-const app = express()
-const port = 3000
+const app = express();
+const port = 5000;
 
 //creating connection with mysql database
 sequelize;
@@ -14,8 +13,8 @@ sequelize;
 app.use(express.json());
 
 //available routes
-app.use('/api/auth',require('./routes/auth'));
+app.use("/api/auth", require("./routes/auth"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,36 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../models/User'); // Import the User model
+const express = require("express"); // Import express
+const router = express.Router(); // Import the router module from express
+const User = require("../models/User"); // Import the User model
+const { body, validationResult } = require("express-validator"); // Import validators
+
+// Validation middleware for user registration
+const validateRegistration = [
+  body("username")
+    .notEmpty()
+    .isLength({ min: 4 })
+    .withMessage("Username is required and must be at least 4 characters long"),
+  body("email").isEmail().withMessage("Invalid email address"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+];
 
 // Route for user registration
 //Create a User using : POST "/api/auth/". Don't require Auth
-router.post('/', async (req, res) => {
+router.post("/", validateRegistration, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   try {
     const { username, email, password } = req.body;
+
+    // Check if the email already exists
+    const existingUser = await User.findOne({ where: { email } });
+    if (existingUser) {
+      return res.status(400).json({ error: "Email ID already exists" });
+    }
 
     // Create a new user using the User model
     const newUser = await User.create({
@@ -15,10 +39,11 @@ router.post('/', async (req, res) => {
       password,
     });
 
+    // You can add additional logic here (e.g., generate tokens, send responses)
     res.status(201).json(newUser); // Respond with the newly created user
   } catch (error) {
-    console.error('Error registering user:', error);
-    res.status(500).json({ error: 'User registration failed' });
+    console.error("Error registering user:", error);
+    res.status(500).json({ error: "User registration failed" });
   }
 });
 
